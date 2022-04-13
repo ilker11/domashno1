c335c4628d98fd88508827a1c178477784a93fd4
ilker11 <ilker01@abv.bg>
2022-04-13 21:26:28 +0300

    bad operand types for binary operator

--- src/com/company/ChanceOfWinning.java
+++ src/com/company/ChanceOfWinning.java
@@ -3,24 +3,24 @@ package com.company;
 import java.util.Random;
 
 public class ChanceOfWinning implements ScratchCard{
-    private double pay;
+    private GooglePay googlePay;
 
-    public ChanceOfWinning(double pay) {
-        this.pay = pay;    }
+    public ChanceOfWinning(GooglePay googlePay) {
+        this.googlePay = googlePay;    }
 
 
 
 
     public void ScratchCard(){
         int c = new Random().nextInt(10);
-        if (pay > 100 && pay < 1000){
+        if (googlePay > 100 && googlePay < 1000){
             if (c < 1) {
                 System.out.println("Congrats you have 10% chance of winning a scratch card");
 
             }
         }
 
-        else if(pay > 1000 && pay < 3000){
+        else if(googlePay > 1000 && googlePay < 3000){
             if (c < 2) {
                 System.out.println("Congrats you have 20% chance of winning a scratch card");
 
@@ -28,7 +28,7 @@ public class ChanceOfWinning implements ScratchCard{
         }
 
 
-       else if (pay > 3000 && pay < 5000){
+       else if (googlePay > 3000 && googlePay < 5000){
            if (c < 3) {
                 System.out.println("Congrats you have 30% chance of winning a scratch card");
 
@@ -36,13 +36,13 @@ public class ChanceOfWinning implements ScratchCard{
         }
 
 
-       else if (pay > 5000 && pay < 10000){
+       else if (googlePay > 5000 && googlePay < 10000){
             if (c < 5) {
                 System.out.println("Congrats you have 50% chance of winning a scratch card");
 
             }
         }
-       else if (pay > 10000){
+       else if (googlePay > 10000){
            if (c < 9) {
                 System.out.println("Congrats you have 100% chance of winning a scratch card");
 
